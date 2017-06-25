c523b8f99b13664c2c6a9c94c6a041fe503a0284
jonathannicolas <hello@gmail.com>
2017-06-26 00:07:26 +0200

    Add fetch

--- index.js
+++ index.js
@@ -12,13 +12,59 @@ const urlMovie6 = "http://localhost:3000/movies/6"
     .catch(error => console.log(error))
 */
 
+/*
+fetch(url)
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(data) {
+        // do stuff with `data`, call second `fetch`
+        return fetch(data.anotherUrl)
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        // do stuff with `data`
+    })
+    .catch(function(error) {
+        console.log('Requestfailed', error)
+    });
+*/
+
+/*
 fetch(urlMovie2, {
     method: "GET"
 })
     .then(response => response.json())
-    .then(data => movie2 = data)
-    .then(json => console.log(json))
-    .catch(error => console.log(error))
+    .then(function(data){
+        let movie2 = data[0];
+
+    })
+
+*/
+
+Promise.all([
+    fetch(urlMovie2, {
+        method: "GET"
+    }),
+    fetch(urlMovie6, {
+        method: "GET"
+    })
+
+]).then((responses) => {
+    console.log(responses);
+    responses.map(function (response) {
+        response.json()
+            .then(json => console.log(json));
+
+    })
+
+})
+
+
+
+
 
 
 
